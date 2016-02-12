205a9d69b227eaa62543204370fdd6691434f378
rpopescu <popescu.roxana.andreea@gmail.com>
2016-02-12 14:16:45 +0200

    first commit

---
+++ app/src/main/java/model/Greeting.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Created by roxanap on 12.02.2016.
+ */
+public class Greeting {
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
